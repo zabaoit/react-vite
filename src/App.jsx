0753987e14e5facda2c425f4b5ce528452f1f681
zabaoit <nguyengiabao8407@gmail.com>
2025-05-18 14:26:07 +0700

    video #27

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,14 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 
-function App() {
+const App = () => {
   const [count, setCount] = useState(0);
+
+  const myFunction = () => {
+    console.log(">>> run arrow function");
+  };
+
+  myFunction();
 
   return (
     <>
@@ -26,6 +32,6 @@ function App() {
       <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
     </>
   );
-}
+};
 
 export default App;
