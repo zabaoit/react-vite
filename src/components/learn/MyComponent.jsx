9dc505df86f21f03a09be98b1d7367f93892c997
zabaoit <nguyengiabao8407@gmail.com>
2025-05-18 15:03:27 +0700

    video #30

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -1,6 +1,11 @@
 // component =  html + css + js
 const MyComponent = () => {
-  return <div>Baobao with react vite</div>;
+  return (
+    <>
+      <div>Baobao with react vite</div>
+      <div className="child">Child</div>
+    </>
+  );
 };
 
 export default MyComponent;
