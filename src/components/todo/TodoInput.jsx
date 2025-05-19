6d32a18a035031cdaa82102e9ebaa48c5bdc5dc1
zabaoit <nguyengiabao8407@gmail.com>
2025-05-19 19:45:56 +0700

    video #36

--- src/components/todo/TodoInput.jsx
+++ src/components/todo/TodoInput.jsx
@@ -1,10 +1,18 @@
 const TodoInput = props => {
   const { addNewTodo } = props;
-  addNewTodo("Baobao");
+  // addNewTodo("Baobao");
+
+  const handleClick = () => {
+    alert("call me");
+  };
+
+  const handlChange = event => {
+    console.log(">>> handlChange: ", event.target.value);
+  };
   return (
     <div className="todo-input">
-      <input type="text" placeholder="Enter your task" />
-      <button>Add</button>
+      <input type="text" placeholder="Enter your task" onChange={() => handlChange} />
+      <button onClick={handleClick}>Add</button>
     </div>
   );
 };
