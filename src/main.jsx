06dda42f9fdebe573dbb6f6bca23fd2a5dce83a8
zabaoit <nguyengiabao8407@gmail.com>
2025-05-22 21:26:37 +0700

    video #46

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import LoginPage from "./pages/login.jsx";
+import RegisterPage from "./pages/register.jsx";
+import UsersPage from "./pages/users.jsx";
+import ProductsPage from "./pages/products.jsx";
+import "./styles/global.css";
 
 const router = createBrowserRouter([
   {
@@ -10,19 +15,19 @@ const router = createBrowserRouter([
   },
   {
     path: "/login",
-    element: <div>login page</div>,
+    element: <LoginPage />,
   },
   {
     path: "/register",
-    element: <div>register page</div>,
+    element: <RegisterPage />,
   },
   {
     path: "/users",
-    element: <div>users page</div>,
+    element: <UsersPage />,
   },
   {
     path: "/products",
-    element: <div>products page</div>,
+    element: <ProductsPage />,
   },
 ]);
 
