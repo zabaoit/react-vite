2db3699825bc02401b1d00be024fa2a48a1f6a9c
zabaoit <nguyengiabao8407@gmail.com>
2025-05-22 16:19:47 +0700

    video #42

--- src/App.jsx
+++ src/App.jsx
@@ -24,13 +24,18 @@ const App = () => {
     return Math.floor(Math.random() * (max - min + 1) + min);
   };
 
+  const deleteTodo = id => {
+    const newTodo = todoList.filter(item => item.id !== id);
+    setTodoList(newTodo);
+  };
+
   return (
     <div className="todo-container">
       <div className="todo-title">Todo list</div>
       <TodoInput addNewTodo={addNewTodo} />
 
       {todoList.length > 0 ? (
-        <TodoData todoList={todoList} />
+        <TodoData todoList={todoList} deleteTodo={deleteTodo} />
       ) : (
         <div className="todo-image">
           <img src={reactLogo} className="logo" />
--- src/components/todo/TodoData.jsx
+++ src/components/todo/TodoData.jsx
@@ -1,13 +1,16 @@
 const TodoData = props => {
-  const { todoList } = props;
+  const { todoList, deleteTodo } = props;
 
+  const handleOnClick = id => {
+    deleteTodo(id);
+  };
   return (
     <div className="todo-data">
       {todoList.map((item, index) => {
         return (
           <div className="todo-item" key={item.id}>
             <div>{item.name}</div>
-            <button>Delete</button>
+            <button onClick={() => handleOnClick(item.id)}>Delete</button>
           </div>
         );
       })}
