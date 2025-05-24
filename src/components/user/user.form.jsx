a49d9bf74c60b21f49d228c42de1b1eb0b8e0d4b
zabaoit <nguyengiabao8407@gmail.com>
2025-05-24 07:51:04 +0700

    video #67

--- src/components/user/user.form.jsx
+++ src/components/user/user.form.jsx
@@ -1,28 +1,61 @@
 import { Button, Input } from "antd";
+import { use } from "react";
+import { useState } from "react";
 
 const UserForm = () => {
+  const [fullName, setFullName] = useState("");
+  const [email, setEmail] = useState("");
+  const [passWord, setPassWord] = useState("");
+  const [phone, setPhone] = useState("");
+
+  const handleClickBtn = () => {
+    console.log(">>> check input: ", { fullName, email, passWord, phone });
+  };
+
   return (
     <div className="user-form" style={{ margin: "20px 0" }}>
       <div style={{ display: "flex", justifyContent: "space-between", padding: "5px" }}>
         <div>
           <span>Full Name</span>
-          <Input />
+          <Input
+            value={fullName}
+            onChange={event => {
+              setFullName(event.target.value);
+            }}
+          />
         </div>
         <div>
           <span>Email</span>
-          <Input />
+          <Input
+            value={email}
+            onChange={event => {
+              setEmail(event.target.value);
+            }}
+          />
         </div>
         <div>
           <span>Password</span>
-          <Input.Password />
+          <Input.Password
+            value={passWord}
+            onChange={event => {
+              setPassWord(event.target.value);
+            }}
+          />
         </div>
         <div>
           <span>Phone number</span>
-          <Input />
+          <Input
+            value={phone}
+            onChange={event => {
+              setPhone(event.target.value);
+            }}
+          />
         </div>
       </div>
       <div style={{ margin: "20px 20px 0" }}>
-        <Button type="primary">Create User</Button>
+        <Button onClick={() => handleClickBtn()} type="primary">
+          Create User
+        </Button>
       </div>
     </div>
   );
