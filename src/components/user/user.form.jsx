e5b67785287604d46f72afc19e974f11f6ebfff3
zabaoit <nguyengiabao8407@gmail.com>
2025-05-24 09:00:48 +0700

    video #70

--- src/components/user/user.form.jsx
+++ src/components/user/user.form.jsx
@@ -1,6 +1,6 @@
-import { Button, Input } from "antd";
+import { Button, Input, notification } from "antd";
 import { useState } from "react";
-import axios from "axios";
+import { createUserAPI } from "../../services/api.service";
 
 const UserForm = () => {
   const [fullName, setFullName] = useState("");
@@ -8,17 +8,14 @@ const UserForm = () => {
   const [password, setPassWord] = useState("");
   const [phone, setPhone] = useState("");
 
-  const handleClickBtn = () => {
-    const URL_BACKEND = "http://localhost:8080/api/v1/user";
-    const data = {
-      fullName: fullName,
-      email: email,
-      password: password,
-      phone: phone,
-    };
-
-    axios.post(URL_BACKEND, data);
-    console.log(">>> check input: ", { fullName, email, passWord, phone });
+  const handleClickBtn = async () => {
+    const res = await createUserAPI(fullName, email, password, phone);
+    if (res.data && res.data) {
+      notification.success({
+        message: "Create user",
+        description: "Tạo mới người dừng thành công",
+      });
+    }
   };
 
   return (
---
+++ src/services/api.service.js
@@ -0,0 +1,17 @@
+// import axios from "axios";
+import axios from "./axios.customize";
+const createUserAPI = (fullName, email, password, phone) => {
+  const URL_BACKEND = "/api/v1/user";
+  const data = {
+    fullName: fullName,
+    email: email,
+    password: password,
+    phone: phone,
+  };
+
+  return axios.post(URL_BACKEND, data);
+};
+
+const updateUserAPI = () => {};
+
+export { createUserAPI, updateUserAPI };
